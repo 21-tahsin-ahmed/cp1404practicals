09373007df6cf5da299e978ef92b0c2f0be29c6f
Tahsin Ahmed <tahsin@example.com>
2026-06-08 18:43:54 +1000

    Added

---
+++ prac_03/randoms.py
@@ -0,0 +1,15 @@
+# What did you see on line 1? - 5,13,5
+# What was the smallest number you could have seen, what was the largest? 5, 19
+
+# What did you see on line 2? 3,7,7,9
+# What was the smallest number you could have seen, what was the largest?  3,9
+# Could line 2 have produced a 4? No
+
+# What did you see on line 3? 5.37, 4.00, 3.30
+# What was the smallest number you could have seen, what was the largest? 2.5,5.4
+
+"""Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+"""
+import random
+
+print(random.randint(1, 101))
